31362a1eaf774cd0d7d9eb9570a9094bbf15b369
trebek1 <trebek08@gmail.com>
2016-10-13 13:57:03 -0700

    added redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,16 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure(); 
+
+store.subscribe(() => {
+	console.log("new state ", store.getState());
+});
+
+store.dispatch(actions.addTodo('go to the bank'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted(1));
 // Load foundation
 $(document).foundation();
 
